c8727d637adf0925685bfd7c325726b4f679a782
Jad Georges <jad.grgs@gmail.com>
2018-02-26 22:09:40 -0800

    added some styling and dried up the nav bar code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :images, dependent: :destroy
+    validates :name, presence: true
+    validates :email, presence:true
+    validates :email, uniqueness: true
+    validates :password, length: { in: 6..20 }
 end

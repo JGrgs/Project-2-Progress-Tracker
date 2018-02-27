fc7131eef9df02b3d34b5a62a056740fa0991267
Jad Georges <jad.grgs@gmail.com>
2018-02-26 16:04:11 -0800

    added edit&update for images and modified associated views

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,25 +1,21 @@
 class ImagesController < ApplicationController
   def index
-    
+    @images = Image.all
   end
 
   def show
     @image = Image.find(params[:id])
+    @user = User.find @image.user_id
   end
 
   def new
-    @image = Image.new
+    @image = current_user.images.new
   end
 
   def create
-    @image = Image.new
-    @image.image_url = params[:image][:image_url]
-    @image.date = params[:image][:date]
-    @image.title = params[:image][:title]
-    @image.description = params[:image][:description]
-    @image.category = params[:image][:category]
+    @image = current_user.images.new image_params
     @image.save
-    redirect_to("/images")
+    redirect_to image_path(@image.id)
   end
 
   def edit
@@ -34,12 +30,17 @@ class ImagesController < ApplicationController
     @image.description = params[:image][:description]
     @image.category = params[:image][:category]
     @image.save
-    redirect_to("/images/#{@image.id}")
+    redirect_to image_path(@image.id)
   end
 
   def destroy
     @image = Image.find(params[:id])
     @image.destroy
     redirect_to("/images")
+  end
+
+  private
+  def image_params
+    return params.require(:image).permit(:image_url, :date, :title, :description, :category)
   end
 end
